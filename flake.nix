98a578099c9f223634310c24d585ee603da96d15
Benjamin Tan <benjamin@dev.ofcr.se>
2022-11-11 17:18:18 +0800

    Update Nix flake inputs

--- flake.nix
+++ flake.nix
@@ -1,18 +1,18 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
-    zig.url = "github:arqv/zig-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=093268502280540a7f5bf1e2a6330a598ba3b7d0";
+    flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f";
+    zig.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, zig }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, zig }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
       in {
        devShell = pkgs.mkShell {
           nativeBuildInputs = [
-            zig.packages."${system}".master.latest
+            zig.packages."${system}"."0.10.0"
           ];
         };
       });
